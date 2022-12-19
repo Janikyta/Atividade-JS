1f612414afc8095fd62f1e4ef5b683ad7eac5711
Janaina Neves <107224223+Janikyta@users.noreply.github.com>
2022-12-19 18:38:30 -0300

    Add files via upload

---
+++ Atividade js.html/ativ.js
@@ -0,0 +1,76 @@
+// Questão 1
+// for(var i = 101; i <=110;i++){
+//    console.log(i);
+//}
+
+//Questão 2
+// const num = [];
+// num[0]= prompt("Insira Valor 1:");
+// num[1]= prompt("Insira Valor 2:");
+// num[2]= prompt("Insira Valor 3:");
+
+// var max = Math.max(...num);
+// console.log(max);
+
+//Questão 3
+// const num = [];
+// num[0]= prompt("Insira Valor 1:");
+// num[1]= prompt("Insira Valor 2:");
+// num[2]= prompt("Insira Valor 3:");
+// var min= Math.min(...num);
+// var max = Math.max(...num);
+// console.log(max);  
+// console.log(min);
+
+//Questão 4
+// var nome= prompt("Insira seu nome:");
+// var cpf= prompt("Insira seu cpf:");
+// var idade= prompt("Insira sua idade:");
+// var datanascimento = prompt("Insira sua data de nascimento:");
+
+
+// console.log(nome);  
+// console.log(cpf);
+// console.log(idade);
+// console.log(datanascimento);
+
+//Questão 5
+// var nomedoproduto= prompt("Insira o nome do produto:");
+// var preço= prompt("Insira o preço:");
+// var estoque= prompt("Item em estoque:");
+// var validade = prompt("Insira a validade:");
+
+
+// console.log(nomedoproduto);  
+// console.log(preço);
+// console.log(estoque);
+// console.log(validade);
+
+// Questão 6
+// var temperaturaFahrenheit = 212;
+// var temperaturaCelsius = (temperaturaFahrenheit - 32.0) / 1.8;
+// console.log(temperaturaCelsius);
+
+//Questão 7
+// var salarioMensalAtual = 3000.50;
+// var percentualReajuste = 15;
+
+// var novoSalario = salarioMensalAtual + (salarioMensalAtual / 100) * percentualReajuste;
+// console.log(novoSalario);
+
+//Questão 8
+
+// var peso = 61;
+// var altura = 1.66;
+
+// var imc = peso / (altura * altura);
+
+// if(imc < 18.5){
+//    console.log('Abaixo do peso.');
+// }else if(imc >= 18.5 && imc <= 25.0){
+//    console.log('Peso normal.');
+// }else if(imc > 25.0 && imc <= 30.0){
+//    console.log('Acima do peso.');
+// }else{
+//    console.log('Obeso.');
+// }
